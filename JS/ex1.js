b12a7cb9ab35886042770a08f244f041cb3a8e78
Raine Felix <rainefelix.lgf@gmail.com>
2020-09-22 17:19:26 -0700

    commit

---
+++ JS/ex1.js
@@ -0,0 +1,14 @@
+/* HOMEWORK 3
+Example 1 JavaScript code
+*/
+
+console.log("Felix's Output from HW 3 Example 1");
+
+const name = prompt("Enter your name:");
+alert(`Hello, ${name}`);
+
+const number = Number(prompt("Number of units completed:"));
+if (number >= 0) {
+  console.log(`${number} is positive`);
+}
+alert(`Your grade standing is `);
